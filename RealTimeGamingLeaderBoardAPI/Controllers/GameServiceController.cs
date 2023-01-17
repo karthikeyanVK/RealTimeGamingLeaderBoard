a2307817eee07d09e454fad33802c04ee4b5866c
Karthikeyan VK <karthik_3030@yahoo.com>
2023-01-17 11:23:17 +0530

    fixed warnings

--- RealTimeGamingLeaderBoardAPI/Controllers/GameServiceController.cs
+++ RealTimeGamingLeaderBoardAPI/Controllers/GameServiceController.cs
@@ -9,7 +9,7 @@ namespace RealTimeGamingLeaderBoardAPI.Controllers
     [ApiController]
     public class GameServiceController : ControllerBase
     {
-        IDatabase db = null;
+        IDatabase? db = null;
         public GameServiceController()
         {
             ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
@@ -41,23 +41,36 @@ namespace RealTimeGamingLeaderBoardAPI.Controllers
         [Route("~/GetLeaderBoard/{topCount}")]
         public IEnumerable<LeaderBoard> GetLeaderBoard(int topCount)
         {
+            if (db == null) return Enumerable.Empty<LeaderBoard>();
             return db.SortedSetRangeByRankWithScores("leaderboard", 0, topCount, Order.Descending).ToList().Select((x, i) => new LeaderBoard { UserId = x.Element.ToString(), Score = (int) x.Score });
         }
         [HttpGet]
         [Route("~/GetLeaderBoardPosition")]
         public int GetLeaderBoardPosition(string userId)
+
         {
-            return (int)db.SortedSetRank("leaderboard", userId).Value; //.ToList().Select((x, i) => new LeaderBoard { UserId = x.Element.ToString(), Score = (int)x.Score });
+            if (db != null)
+            {
+                var position = db.SortedSetRank("leaderboard", userId, Order.Descending);
+                if (position.HasValue)
+                    return (int)position.Value;
+            }
+
+            return -1;
         }
         [HttpGet]
         [Route("~/GetLeaderBoardRelativePosition")]
-        public IEnumerable<LeaderBoard> GetLeaderBoardRelativePosition(string userId, int FetchAboveNBelow)
+        public IEnumerable<LeaderBoard>? GetLeaderBoardRelativePosition(string userId, int FetchAboveNBelow)
             
          {
-            int rank = (int)db.SortedSetRank("leaderboard", userId).Value;
-            string temp = db.SortedSetRank("leaderboard", rank).ToString();
-            var positionList = db.SortedSetRangeByRank("leaderboard", rank - 4, rank + 4, Order.Descending);
-            return positionList.ToList().Select((x, i) => new LeaderBoard { UserId = x.ToString(), Score = (int)db.SortedSetScore("leaderboard", x) });
+            if (db == null) return Enumerable.Empty<LeaderBoard>();
+            var rank = db.SortedSetRank("leaderboard", userId);
+            if (rank != null)
+            {
+                var positionList = db.SortedSetRangeByRank("leaderboard", rank.Value - 4, rank.Value + 4, Order.Descending);
+                return positionList.ToList().Select((x, i) => new LeaderBoard { UserId = x.ToString(), Score = (int)db.SortedSetScore("leaderboard", x) });
+            }
+            return null;
         }
         
     }
